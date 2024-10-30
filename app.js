bd0dea1d648d76956072adb1c24b6f1c60383002
ErGrei <i.s.piskunov@gmail.com>
2024-10-30 21:06:41 +0400

    feat: add device

--- app.js
+++ app.js
@@ -35,3 +35,86 @@ const devices = [
   let newDeviceId = 4;
   let newNodeId = 10;
   
+function renderApp (){
+    const divaseList = document.getElementById('deviceList');
+    divaseList.innerHTML = '';
+    devices.forEach(device => divaseList.appendChild(createDivaseElement(device)))
+};
+
+function createDivaseElement (device){
+    const deviceDiv = document.createElement('div');
+    deviceDiv.className = 'device-item';
+
+    const devaseHeader = createDevaseHeader(device);
+    deviceDiv.appendChild(devaseHeader);
+
+    const createNode = createNodeElement(device);
+    deviceDiv.appendChild(createNode);
+
+    return deviceDiv
+}
+
+function createDevaseHeader (device){
+    const devaseHeader = document.createElement('div');
+    devaseHeader.className = 'device-header';
+
+    if (device.isEditing) {
+        const editInput = document.createElement('input');
+        editInput.value = device.name;
+
+        editInput.oninput = (e) => {device.name = e.target.value};
+        devaseHeader.appendChild(editInput)
+
+        const saveButton = document.createElement('button');
+        saveButton.innerText = 'Сохранить';
+        devaseHeader.appendChild(saveButton);
+        saveButton.onclick = () => {
+            device.isEditing = false;
+            renderApp();
+        } 
+    } else {
+        const deviceName = document.createElement('span');
+        deviceName.innerText = device.name;
+        devaseHeader.appendChild(deviceName)
+
+        const editButton = document.createElement('button');
+        editButton.innerText = 'Редактировать';
+        devaseHeader.appendChild(editButton);
+
+        editButton.onclick = () => {
+            device.isEditing = true;
+            renderApp();
+        } 
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Удалить';
+        devaseHeader.appendChild(deleteButton);
+
+        deleteButton.onclick = () => {
+            const index = devices.findIndex(e => e.id === device.id );
+            if (index !== -1) {
+                devices.splice(index, 1);
+            }
+            renderApp();
+        } 
+    }
+    return devaseHeader
+
+}
+
+function createNodeElement() {
+const nodeDiv = document.createElement('div');
+nodeDiv.className = 'add-node';
+
+nodeDiv.innerHTML = ` <input type='text' placeholder = 'Название узла'>
+<button onclick = 'addNode()'>Добавить узел</button>
+`
+
+
+return nodeDiv
+}
+
+function addNode(){
+console.log('работает')
+}
+renderApp ()
